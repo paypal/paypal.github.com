e19975066de5c375a988cdb5c924e08c61415765
lmarkus <lmarkus@paypal.com>
2013-11-19 12:08:22 -0800

    fixed project links

--- projects.js
+++ projects.js
@@ -13,88 +13,88 @@ PAYPAL.projects=[
         "name": "Aurora",
         "language":"javascript",
         "desc": "Project for PayPal Cloud Open Source Code",
-        "repo": "https:/hub.com/paypal/aurora"
+        "repo": "https://github.com/paypal/aurora"
     },
     {
         "name": "KrakenJS",
         "language":"javascript",
         "desc": "An enterprise-grade NodeJS application framework.",
-        "repo": "https:/hub.com/paypal/webcore"
+        "repo": "https://github.com/paypal/kraken-js"
     },
     {
         "name": "Genio",
         "language":"ruby",
         "desc": "Code generation tool to consume APIs in multiple programming languages.",
-        "repo": "https:/hub.com/paypal/webcore"
+        "repo": "https://github.com/paypal/genio"
     },
 
     {
         "name": "Generator-kraken",
         "language":"javascript",
         "desc": "A Yeoman generator for Kraken",
-        "repo": "https:/hub.com/paypal/generator-webcore"
+        "repo": "https://github.com/paypal/generator-webcore"
     },
 
     {
         "name": "Kraken-devtools",
         "language":"javascript",
         "desc": "Supporting tools for Kraken",
-        "repo": "https:/hub.com/paypal/kraken-devtools"
+        "repo": "https://github.com/paypal/kraken-devtools"
     },
 
     {
         "name": "Lusca",
         "language":"javascript",
         "desc": "Application Security for Express",
-        "repo": "https:/hub.com/paypal/lusca"
+        "repo": "https://github.com/paypal/lusca"
     },
 
     {
         "name": "Adaro",
         "language":"javascript",
         "desc": "DustJS support for express",
-        "repo": "https:/hub.com/paypal/adaro"
+        "repo": "https://github.com/paypal/adaro"
     },
 
     {
         "name": "Makara",
         "language":"javascript",
         "desc": "i18n support",
-        "repo": "https:/hub.com/paypal/makara"
+        "repo": "https://github.com/paypal/makara"
     },
 
     {
         "name": "Kappa",
         "language":"javascript",
         "desc": "An NPM Proxy",
-        "repo": "https:/hub.com/paypal/kappa"
+        "repo": "https://github.com/paypal/kappa"
     },
 
     {
         "name": "Express-enrouten",
         "language":"javascript",
         "desc": "An express route initialization and configuration module.",
-        "repo": "https:/hub.com/paypal/express-enrouten"
+        "repo": "https://github.com/paypal/express-enrouten"
     },
 
     {
         "name": "FindATag",
         "language":"javascript",
         "desc": "A specialized tag parser for dust",
-        "repo": "https:/hub.com/paypal/findatag"
+        "repo": "https://github.com/paypal/findatag"
     },
 
     {
         "name": "Spud",
         "language":"javascript",
         "desc": "A content bundle transcoder",
-        "repo": "https:/hub.com/paypal/spud"
+        "repo": "https://github.com/paypal/spud"
     },
 
     {
         "name": "Shortstop",
         "language":"javascript",
         "desc": "Enables use of protocols in configuration.",
-        "repo": "https:/hub.com/paypal/shortstop"
+        "repo": "https://github.com/paypal/shortstop"
     }
 ]
