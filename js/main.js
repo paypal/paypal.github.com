c1a3154393c9c9c179246ac6c2e0e6bad8933ab7
lmarkus <lmarkus@paypal.com>
2013-12-20 12:38:09 -0800

    Minor tweaks, plus Adaptive

--- js/main.js
+++ js/main.js
@@ -6,47 +6,35 @@
  * To change this template use File | Settings | File Templates.
  */
 
-$(function(){
+$(function () {
+        var uri = 'https://api.github.com/orgs/paypal/repos?callback=?'
+            + '&per_page=100';
 
-    var uri = "https://api.github.com/orgs/paypal/repos?callback=?"
-        + "&per_page=100";
+        function createBlock(repo) {
+            var block = $('<div></div>').addClass('project');
 
-   /*
-   //Auto populate option
-    $.getJSON(uri, function (response) {
-        var repos = response.data;
-        for(var i in repos){
-            createBlock(repos[i]);
+            var name = $('<h2></h2>').html(repo.name);
+            var lang = $('<div></div>').html(repo.language).addClass(repo.language + ' language');
+            var desc = $('<p></p>').html(repo.desc);
+            var link = $('<a>&gt;</a>')
+                .attr('href', repo.repo)
+                .attr('aria-label', 'View ' + repo.name + ' on GitHub')
+                .attr('target', '_BLANK')
+                .addClass(repo.language);
+            ;
+            block
+                .append(name)
+                .append(lang)
+                .append(desc)
+                .append(link)
 
+            $('#repos').append(block);
+
+        };
+
+        //List Option
+        for (var i in PAYPAL.projects) {
+              createBlock(PAYPAL.projects[i]);
         }
-        //console.log(data);
-    })
-     */
-
-    //List Option
-    for(var i in PAYPAL.projects){
-        createBlock(PAYPAL.projects[i]);
-    }
-    $('#repos').append($("<div>&nbsp</div>"));
-
-    function createBlock(repo){
-        var block=$("<div></div>").addClass("project");
-        var name=$("<h2></h2>").html(repo.name);
-        var lang=$("<div></div>").html(repo.language).addClass(repo.language+ " language");
-        var desc=$("<p></p>").html(repo.desc);
-        var link=$("<a>&gt;</a>")
-            .attr('href',repo.repo)
-            .attr('aria-label   ',"View "+repo.name+" on GitHub")
-            .attr('target','_BLANK')
-            .addClass(repo.language);;
-        block
-            .append(name)
-            .append(lang)
-            .append(desc)
-            .append(link)
-
-        $('#repos').append(block);
-
-    }
-
+        $('#repos').append($('<div>&nbsp</div>'));
 })
